fe627407a7cb1e0db939a39c7db1101b404a29ad
Vasu Nagendra <vasu@termtegrity.com>
2019-10-09 15:26:39 -0500

    Build a 11.5 container. Also note `pg_config --version` is a very confusing way to check version. That reports `12.xx` which is quite surprising. However `pg_ctl --version` reports the right thing. Since we are correctly installing from apt, `postgresql-11` and all the directories are saying the right things, we should be okay

---
+++ 11.5/start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+# if data file exists then no volumes have been mounted, otherwise start fresh
+#service postgresql stop
+
+if [ ! -f /var/lib/postgresql/$PG_VERSION/main/PG_VERSION ]; then
+    echo "Running intidb..."
+    su postgres -c "initdb --pgdata ${PGDATA} -A peer";
+    cp -r /etc/postgresql/11/main/* /var/lib/postgresql/$PG_VERSION/main/
+    chown -Rf postgres:postgres ${PGDATA}
+    chmod 700 ${PGDATA}
+fi
+
+# Stop postgres when exiting
+trap "service postgresql stop; exit" SIGHUP SIGINT SIGTERM
+
+## Start postgres
+echo "Start Postgres and wait for start to finish ..."
+su postgres -c "pg_ctl start -w"
+
+# UTF8
+su postgres -c "psql -c \"UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';\"";
+su postgres -c "psql -c \"DROP DATABASE template1;\"";
+su postgres -c "psql -c \"CREATE DATABASE template1 WITH template = template0 encoding = 'UTF8';\"";
+su postgres -c "psql -c \"UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';\"";
+su postgres -c "psql -d template1 -c \"VACUUM FREEZE;\""
+
+# Create user and database
+su postgres -c "psql -c \"CREATE USER docker WITH SUPERUSER PASSWORD 'docker';\"" || true;
+su postgres -c "createdb --encoding=UTF8 --owner=docker docker" || true;
+
+echo "Looping..."
+while true; do sleep 1; done
